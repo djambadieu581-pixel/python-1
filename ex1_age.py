6671cbe34b77c75bb2b4881649b63f4cac7a5201
djambadieu581-pixel <djambadieu581@gmail.com>
2026-09-22 20:44:04 +0100

    La revue est integree

--- ex1_age.py
+++ ex1_age.py
@@ -1,5 +1,6 @@
 nom = input("quel est votre nom ? ")
-annee_naissance = int(input("quelle est votre annee de naissance ? "))
+annee_naissance = input("quelle est votre annee de naissance ? ")
+annee_naissance_convertit = int(annee_naissance)
 annee_actuelle = 2026
 age = annee_actuelle - annee_naissance
 age = annee_actuelle - annee_naissance 
